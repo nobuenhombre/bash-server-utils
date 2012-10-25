ccb126f1cb3fec7be3575ed360e1d6d7527490c5
Sukharev Ivan <nobuenhombre@yandex.ru>
2012-10-25 22:30:21 +0400

    скрипт импорта базы данны�

---
+++ mysql/import.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source /home/SCRIPTS/config.cfg
+
+for db in ${DBLIST[@]}
+do
+	if [-f "$DIRDBRESTORE/$db.zip"];
+	then
+		cd $DIRDBRESTORE
+		unzip $db.zip
+		rm "$DIRDBRESTORE/$db.zip"
+	fi
+	if [-f "$DIRDBRESTORE/$db.sql"];
+	then
+		mysql --user=$MYSQLUSER --password=MYSQLPASS $db < $DIRDBRESTORE/$db.sql
+		rm "$DIRDBRESTORE/$db.sql"
+	fi	
+done
