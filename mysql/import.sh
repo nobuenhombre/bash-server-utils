18fe7f20e8fe407b898905203495e29316177e36
nobuenhombre <nobuenhombre@yandex.ru>
2012-11-08 11:13:49 +0400

    bug fix
    синтаксическая ошибка.

--- mysql/import.sh
+++ mysql/import.sh
@@ -3,13 +3,13 @@ source /home/SCRIPTS/config.cfg
 
 for db in ${DBLIST[@]}
 do
-	if [-f "$DIR_UPLOAD_DB/$db.zip"];
+	if [ -f "$DIR_UPLOAD_DB/$db.zip" ];
 	then
 		cd $DIR_UPLOAD_DB
 		unzip $db.zip
 		rm "$DIR_UPLOAD_DB/$db.zip"
 	fi
-	if [-f "$DIR_UPLOAD_DB/$db.sql"];
+	if [ -f "$DIR_UPLOAD_DB/$db.sql" ];
 	then
 		mysql --user=$MYSQL_USER --password="$MYSQL_PASS" $db < $DIR_UPLOAD_DB/$db.sql
 		rm "$DIR_UPLOAD_DB/$db.sql"
