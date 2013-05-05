27fcdf6137d280cca89638299ae1907f9d43b0ae
Sukharev Ivan <nobuenhombre@yandex.ru>
2013-05-05 19:54:04 +0400

    reorganization

--- lamp/makelinks.sh
+++ lamp/makelinks.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+mkdir /home/SCRIPTS/lamp/configs
 ln -s /etc/httpd/conf.d/vhosts.conf /home/SCRIPTS/lamp/configs/vhosts.conf
 ln -s /etc/httpd/conf/httpd.conf /home/SCRIPTS/lamp/configs/httpd.conf
 ln -s /etc/php.ini /home/SCRIPTS/lamp/configs/php.ini
